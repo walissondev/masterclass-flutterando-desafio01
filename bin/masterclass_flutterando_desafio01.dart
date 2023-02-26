fdc3905cc5bad108ff1120530d12c33cac7247bc
Walisson Almeida Dos Santos <walisson@hostaraguaia.com.br>
2023-02-26 15:29:29 -0300

    adiciona git

---
+++ bin/masterclass_flutterando_desafio01.dart
@@ -0,0 +1,5 @@
+import 'package:masterclass_flutterando_desafio01/masterclass_flutterando_desafio01.dart' as masterclass_flutterando_desafio01;
+
+void main(List<String> arguments) {
+  print('Hello world: ${masterclass_flutterando_desafio01.calculate()}!');
+}
---
+++ test/masterclass_flutterando_desafio01_test.dart
@@ -0,0 +1,8 @@
+import 'package:masterclass_flutterando_desafio01/masterclass_flutterando_desafio01.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
